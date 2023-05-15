0dde7c8de88f9683afcc1e4b3093cbac029b8696
Patrickhell <patrickhell19@yahoo.fr>
2023-05-15 10:03:40 +0200

    aggiunto toFixed al risultato

--- js/myscript.js
+++ js/myscript.js
@@ -1,14 +1,14 @@
 const userAdultAge = 18;
 const costByKm = (0.21 )
-const numberOfKm = prompt("Quanti  vorrebbe percorrere?");
+const numberOfKm = prompt("Quanti Kilometri vorrebbe percorrere?");
 const travelCost = (numberOfKm * (0.21 )) ;
 const passengerAge = parseInt(prompt("Quanti Anni ha?"));
 let result;
 
 if(passengerAge < 18) {
-    result = travelCost - ((20/100) * travelCost ).toFixed(1) ;
+    result = travelCost - ((20/100) * travelCost ).toFixed(2) ;
 } else if(passengerAge > 65) {
-    result = travelCost - ((40/100) * travelCost ) .toFixed(0) ;
+    result = travelCost - ((40/100) * travelCost ) .toFixed(2) ;
 } else {
     result = travelCost .toFixed(2);
 }
